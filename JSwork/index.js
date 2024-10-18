05c2658b015957188d18094661fda8fe15e653cc
AnikKushwaha <anikkushwaha05@gmail.com>
2024-10-18 13:10:07 +0530

    js work

---
+++ JSwork/index.js
@@ -0,0 +1,14 @@
+// console.log("Hello World")
+
+// const a=12;
+// if(a>10){
+//    let b=10;
+//    a=40;
+//     console.log("a inside:"+a);
+// }
+// console.log("a outside:"+b);
+function fun(a,b){
+    console.log("Hiiii")
+}
+const result=fun();
+console.log(result);
